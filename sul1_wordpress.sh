73c069d01878ee17842ab8bde7f3dea38befc4c7
sultan8898 <106063190+sultan8898@users.noreply.github.com>
2023-08-28 00:53:34 +0500

    Update sul1_wordpress.sh

--- sul1_wordpress.sh
+++ sul1_wordpress.sh
@@ -1,55 +1,59 @@
 #!/bin/bash
-##This script goes through all of the access logs and writes down IPs that have accessed the site in the last month. It can be adjusted for any specifc day as well, if * .gz is replaced
-##specific day is chosen as well. 
-## The bot script is not complete inside it, if anyone wants to work on it, feel free to do so.
-##Written by Sultan
-# Get the WordPress server name from nginx config
-server_name=$(awk '/^ *server_name/ && /wordpress/ {print $2; exit}' ../conf/server.nginx)
 
-# Extract the code from the server name
-code=$(echo "$server_name" | sed 's/wordpress-\([^-]*\)-\([^-]*\)\..*/\1-\2/')
+# Default paths
+plugins_path="./wp-content/plugins"
+themes_path="./wp-content/themes"
+log_path="../logs/php-app.slow.log"
 
-# Create traffic.txt file
-touch ../public_html/traffic.txt
+# Get a list of all plugins and themes
+plugins=$(find "$plugins_path" -maxdepth 1 -type d | sed 's|.*/||')
+themes=$(find "$themes_path" -maxdepth 1 -type d | sed 's|.*/||')
 
-# Extract IP addresses from logs and count occurrences
-cat "../logs/apache_wordpress-$code.com.access.log" "../logs/apache_wordpress-$code.com.access.log.1" \
-  | awk '{print $1}' \
-  | sort \
-  | uniq -c \
-  | sort -rn \
-  > ../public_html/traffic.txt
+# Initialize associative arrays to store the counts of each plugin and theme
+declare -A plugin_counts
+declare -A theme_counts
 
-# Extract bot names from logs and count occurrences
-cat "../logs/apache_wordpress-$code.com.access.log" "../logs/apache_wordpress-$code.com.access.log.1" \
-  | awk 'tolower($0) ~ /bot/ {for (i=1;i<=NF;i++) if ($i ~ /bot$/) {print $(i-1); break}}' \
-  | sort \
-  | uniq -c \
-  | sort -rn \
-  >> ../public_html/traffic.txt
+# Loop through each plugin and search for entries in the slow logs
+for plugin in $plugins; do
+  # Search the slow logs for entries related to the current plugin and extract the plugin name
+  log_entries=$(grep -o "plugins/$plugin/." "$log_path" | sed 's/plugins\/.\///')
 
-# Extract IP addresses from gzipped logs and count occurrences
-zgrep -h --no-filename '^[^ ]\+' "../logs/apache_wordpress-$code.com.access.log."{2..31}".gz" \
-  | awk '{print $1}' \
-  | sort \
-  | uniq -c \
-  | sort -rn \
-  >> ../public_html/traffic.txt
+  # If there are log entries, print them and update the count for the plugin
+  if [ -n "$log_entries" ]; then
+    echo "Entries for plugin $plugin:"
+    echo "$log_entries"
+    echo ""
+    for entry in $log_entries; do
+      (( plugin_counts[$plugin]++ ))
+    done
+  fi
+done
 
-# Extract bot names from gzipped logs and count occurrences
-zgrep -h --no-filename -i 'bot.*http' "../logs/apache_wordpress-$code.com.access.log."{2..31}".gz" \
-  | awk '{for (i=1;i<=NF;i++) if ($i ~ /bot$/) {print $(i-1); break}}' \
-  | sort \
-  | uniq -c \
-  | sort -rn \
-  >> ../public_html/traffic.txt
+# Loop through each theme and search for entries in the slow logs
+for theme in $themes; do
+  # Search the slow logs for entries related to the current theme and extract the theme name
+  log_entries=$(grep -o "themes/$theme/." "$log_path" | sed 's/themes\/.\///')
 
-# Count total number of IP addresses
-total=$(awk '{s+=$1} END {print s}' ../public_html/traffic.txt)
+  # If there are log entries, print them and update the count for the theme
+  if [ -n "$log_entries" ]; then
+    echo "Entries for theme $theme:"
+    echo "$log_entries"
+    echo ""
+    for entry in $log_entries; do
+      (( theme_counts[$theme]++ ))
+    done
+  fi
+done
 
-# Count total number of unique IP addresses
-unique=$(wc -l < ../public_html/traffic.txt)
+# Print the top N plugins and themes with the highest counts
+echo "Top plugins by count:"
+for plugin in "${!plugin_counts[@]}"; do
+  echo "${plugin_counts[$plugin]} $plugin"
+done | sort -rn | head -n 10
 
-# Print total and unique at the end of the output
-echo "Total number of IP addresses: $total" >> ../public_html/traffic.txt
-echo "Total number of unique IP addresses: $unique" >> ../public_html/traffic.txt
+echo "Top themes by count:"
+for theme in "${!theme_counts[@]}"; do
+  echo "${theme_counts[$theme]} $theme"
+done | sort -rn | head -n 10
+
+
